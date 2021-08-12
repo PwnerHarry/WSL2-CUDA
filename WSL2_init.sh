c790098046cef771449d8fa578b9ac79f81ba1e2
Harry Zhao <mingdeharry@outlook.com>
2021-08-12 14:11:20 -0700

    Update WSL2_init.sh

--- WSL2_init.sh
+++ WSL2_init.sh
@@ -8,6 +8,9 @@ apt install -y cuda-toolkit-11-2 cuda-toolkit-10-2
 apt install -y libcudnn7=7.*-1+cuda10.2
 apt install -y libcudnn8=8.*-1+cuda11.2
 
+apt-mark hold libcudnn7
+apt-mark hold libcudnn8
+
 apt install -y python3.8 python3.8-dev python3.8-venv python3.8-distutils
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.8 get-pip.py
