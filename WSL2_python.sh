c961a741acd77af900e13368e3d9da0b9ff28f4d
Harry Zhao <mingdeharry@outlook.com>
2021-09-30 10:08:20 -0400

    Update WSL2_python.sh

--- WSL2_python.sh
+++ WSL2_python.sh
@@ -4,6 +4,3 @@ python3.8 -m venv ENV
 source ~/ENV/bin/activate
 pip install --upgrade pip
 pip install tensorflow
-
-# for tensorflow's libcusolver.so.11 not found error
-sudo ln -s /usr/local/cuda-11.2/targets/x86_64-linux/lib/libcusolver.so.11 ~/ENV/lib/python3.8/site-packages/tensorflow/python/libcusolver.so.11
